52e03ddd73c51887b366a53fcc41d23dfaca10af
YanFan Hw <yanfanhw@gmail.com>
2022-01-24 13:26:09 +0700

    Design Trello Frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,87 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='hello-word'>Hello word!</h1>
+    <div className="trello-clone-container">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://img.thuthuatphanmem.vn/uploads/2018/09/27/wallpaper-4k_105912678.jpg" alt="trello-alt-img" />
+              Title: Trello Clone Web</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://img.thuthuatphanmem.vn/uploads/2018/09/27/wallpaper-4k_105912678.jpg" alt="trello-alt-img" />
+              Title: Trello Clone Web</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://img.thuthuatphanmem.vn/uploads/2018/09/27/wallpaper-4k_105912678.jpg" alt="trello-alt-img" />
+              Title: Trello Clone Web</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://img.thuthuatphanmem.vn/uploads/2018/09/27/wallpaper-4k_105912678.jpg" alt="trello-alt-img" />
+              Title: Trello Clone Web</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://img.thuthuatphanmem.vn/uploads/2018/09/27/wallpaper-4k_105912678.jpg" alt="trello-alt-img" />
+              Title: Trello Clone Web</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
